f318ec7eeaee6cb4d350d4966a549482ff6cd5e5
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-05-18 23:50:23 +0300

    Implement presale

--- contracts/AdvancedNft.sol
+++ contracts/AdvancedNft.sol
@@ -3,8 +3,15 @@ pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "hardhat/console.sol";
 
 contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
+  uint256 private immutable _cap;
+
+  using Counters for Counters.Counter;
+  Counters.Counter internal _tokenCounter;
+
   enum Stages {
     Inactive,
     PreSale,
@@ -18,8 +25,16 @@ contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
 
   Stages public stage;
 
-  constructor() {
+  bytes32 public immutable merkleRoot;
+
+  constructor(bytes32 merkleRoot_, uint256 _cap) {
+    require(merkleRoot_ != 0);
+    require(cap_ > 0, "ERC721Capped: cap is 0");
+
+    _cap = cap_;
+
     stage = Stages.Inactive;
+    merkleRoot = merkleRoot_;
   }
 
   modifier atStage(Stages stage_) {
@@ -33,5 +48,30 @@ contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
   function activatePresale() public onlyOwner atStage(Stages.Inactive) {
     stage = Stages.PreSale;
     emit StageTransition(uint256(Stages.Inactive), uint256(Stages.PreSale));
+  }
+
+  function _mint(address _to, uint256 _tokenId) internal virtual override {
+    require(_tokenCounter.current() < _cap, "ERC721Capped: cap exceeded");
+    super._mint(_to, _tokenId);
+  }
+
+  function presaleMint(uint256 _ticket, bytes32[] calldata _proof) public atStage(Stages.PreSale) {
+    require(_ticket != 0, "Ticket not provided");
+    require(_proof.length != 0, "Proof is empty");
+
+    bytes32 leaf = merkleLeaf(_ticket);
+    require(MerkleProof.verify(_proof, merkleRoot, leaf), "Invalid proof");
+
+    uint256 _tokenId = _tokenCounter.current();
+
+    _tokenCounter.increment();
+
+
+    _safeMint(msg.sender, _tokenId);
+    // TODO: refactor: implement commit-reveal pattern for idx, instead of taking it from args
+  }
+
+  function merkleLeaf(uint256 _ticket) public view returns(bytes32) {
+    return keccak256(abi.encode(msg.sender, _ticket));
   }
 }
--- test/AdvancedNft.js
+++ test/AdvancedNft.js
@@ -1,15 +1,61 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { MerkleTree } = require("merkletreejs");
+const { utils } = ethers;
 
 describe("Advanced NFT", function () {
+  function buildMerkleWhitelist(accounts) {
+    const leafNodes = accounts.map((account, idx) => {
+      const paddedAddress = utils.hexZeroPad(account, 32).toLowerCase();
+      // const addressBytes = utils.arrayify(utils.hexZeroPad(account, 32).toLowerCase());
+      // // const addressBytes = utils.arrayify(utils.getAddress(account));
+      const ticketBytes = utils.hexZeroPad(utils.hexValue(idx + 1), 32).toLowerCase();
+
+      const beforeHashing = Buffer.from(utils.concat([paddedAddress, ticketBytes]));
+      const asdf = utils.keccak256(beforeHashing);
+
+      return asdf;
+    }).sort();
+
+    const merkleTree = new MerkleTree(leafNodes, utils.keccak256, { sortPairs: true });
+
+    return { leafNodes, merkleTree };
+  }
+
+  function getTokenFactory() {
+    return ethers.getContractFactory("AdvancedNft");
+  }
+
   async function deployToken() {
-    const [firstAccount, secondAccount] = await ethers.getSigners();
+    const [
+      firstAccount,
+      secondAccount,
+      thirdAccount,
+      fourthAccount,
+      fifthAccount
+    ] = await ethers.getSigners();
 
-    const TokenFactory = await ethers.getContractFactory("AdvancedNft");
-    const token = await TokenFactory.deploy();
+    const TokenFactory = await getTokenFactory();
+    const { merkleTree, leafNodes } = buildMerkleWhitelist([
+      secondAccount.address,
+      thirdAccount.address,
+      fourthAccount.address,
+      fifthAccount.address
+    ]);
 
-    return { token, firstAccount, secondAccount};
+    // Max supply: 6 available, 4 whitelist spots
+    const token = await TokenFactory.deploy(merkleTree.getRoot(), 6);
+
+    return {
+      token,
+      firstAccount,
+      secondAccount,
+      fourthAccount,
+      fifthAccount,
+      merkleTree,
+      leafNodes
+    };
   }
 
   describe("Deployment", function () {
@@ -25,10 +71,34 @@ describe("Advanced NFT", function () {
       expect(await token.symbol()).to.equal('ADV');
     });
 
+    it("reverts if trying to deploy without supply", async function() {
+      const { token } = await loadFixture(deployToken);
+      const TokenFactory = await getTokenFactory();
+
+      expect(
+        TokenFactory.deploy(utils.constants.AddressZero)
+      ).to.be.reverted;
+    });
+
     it("initializes contract to inactive stage", async function () {
       const { token } = await loadFixture(deployToken);
 
       expect(await token.stage()).to.equal(0);
+    });
+
+    it("reverts unless merkle root provided", async function () {
+      const { token } = await loadFixture(deployToken);
+      const TokenFactory = await getTokenFactory();
+
+      expect(TokenFactory.deploy()).to.be.reverted;
+    });
+
+    it("returns merkle root", async function() {
+      const { token, merkleTree } = await loadFixture(deployToken);
+
+      expect(await token.merkleRoot()).to.equal(
+        `0x${merkleTree.getRoot().toString('hex')}`
+      );
     });
   });
 
@@ -55,6 +125,82 @@ describe("Advanced NFT", function () {
         "FunctionInvalidAtThisStage"
       );
     });
+
+    it("reverts when trying to presale mint while it has not been activated", async function() {
+      const { token } = await loadFixture(deployToken);
+
+      const dummyTicketNumber = 123;
+      const dummyProofs = [];
+
+      await expect(token.presaleMint(dummyTicketNumber, dummyProofs)).to.be.revertedWithCustomError(
+        token,
+        "FunctionInvalidAtThisStage"
+      );
+    });
+
+    it("reverts if zero ticket number provided", async function () {
+      const { token } = await loadFixture(deployToken);
+
+      // TODO: expect full state transitions and emitted events
+      await (await token.activatePresale()).wait();
+
+      await expect(
+        token.presaleMint(0, [1, 2, 3])
+      ).to.be.revertedWith("Ticket not provided");
+    });
+
+    it("reverts if empty proof provided", async function () {
+      const { token } = await loadFixture(deployToken);
+
+      await (await token.activatePresale()).wait();
+
+      await expect(
+        token.presaleMint(1, [])
+      ).to.be.revertedWith("Proof is empty");
+    });
+
+    it("reverts if could not prove due to ticket number for another address", async function () {
+      const { token, merkleTree, leafNodes, firstAccount, secondAccount } = await loadFixture(deployToken);
+
+      await (await token.activatePresale()).wait();
+
+      const proofs = merkleTree.getHexProof(leafNodes[0]);
+
+      // NOTE: secondAccount should have a ticket number of 1, 3 is invalid
+      await expect(
+        token.connect(secondAccount).presaleMint(3, proofs)
+      ).to.be.revertedWith("Invalid proof");
+    });
+
+    it.only("reverts if could not prove due to one proofs being incorrect", async function () {
+      const { token, merkleTree, leafNodes, firstAccount, secondAccount } = await loadFixture(deployToken);
+
+      await (await token.activatePresale()).wait();
+      const proofs = merkleTree.getHexProof(leafNodes[0]);
+
+      const fakeProof = `0x999${proofs[1].substring(5)}`;
+
+      // NOTE: secondAccount should have a ticket number of 1
+      await expect(
+        token.connect(secondAccount).presaleMint(1, [proofs[0], fakeProof])
+      ).to.be.revertedWith("Invalid proof");
+    });
+//     it("mints a token for an address given presale ticket number and proofs", async function() {
+//       const { token, firstAccount } = await loadFixture(deployToken);
+//       const doesNotMatterTicketNumber = 123;
+//       const proofs = [];
+
+//       expect(await token.mint()).to.emit(token, "Transfer").withArgs(
+//         firstAccount.address,
+//         ethers.constants.AddressZero,
+//         anyValue
+//       );
+//     });
+//     it("reverts if trying to re-use ticket number and proofs", async function() {
+
+    // TODO: deal with commit-reveal ids
+    //     it("reverts mint if ", async function() {
+    //     });
   });
 });
 
