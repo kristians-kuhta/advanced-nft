e02e0ad7b1f3e9be090d48713e7105629387f003
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-05-19 00:26:50 +0300

    WIP

--- contracts/AdvancedNft.sol
+++ contracts/AdvancedNft.sol
@@ -26,8 +26,11 @@ contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
   Stages public stage;
 
   bytes32 public immutable merkleRoot;
+  uint256 public constant MINT_PRICE = 1 * 10**18;
+  address private immutable developer1;
+  address private immutable developer2;
 
-  constructor(bytes32 merkleRoot_, uint256 _cap) {
+  constructor(bytes32 merkleRoot_, uint256 _cap, address developer1_, address developer2_) {
     require(merkleRoot_ != 0);
     require(cap_ > 0, "ERC721Capped: cap is 0");
 
@@ -35,6 +38,9 @@ contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
 
     stage = Stages.Inactive;
     merkleRoot = merkleRoot_;
+
+    developer1 = developer1_;
+    developer2 = developer2_;
   }
 
   modifier atStage(Stages stage_) {
@@ -45,9 +51,22 @@ contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
     _;
   }
 
+  function _requireDeveloper(address receiver_) internal {
+    if (receiver_ != developer1 && receiver_ != developer2) {
+      revert('Not a developer');
+    }
+  }
+
   function activatePresale() public onlyOwner atStage(Stages.Inactive) {
     stage = Stages.PreSale;
+
     emit StageTransition(uint256(Stages.Inactive), uint256(Stages.PreSale));
+  }
+
+  function activatePublicSale() public onlyOwner atStage(Stages.PreSale) {
+    stage = Stages.PublicSale;
+
+    emit StageTransition(uint256(Stages.PreSale), uint256(Stages.PublicSale));
   }
 
   function _mint(address _to, uint256 _tokenId) internal virtual override {
@@ -62,6 +81,7 @@ contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
     bytes32 leaf = merkleLeaf(_ticket);
     require(MerkleProof.verify(_proof, merkleRoot, leaf), "Invalid proof");
 
+    // TODO: use bitmaps for registering used presale tickets
     uint256 _tokenId = _tokenCounter.current();
 
     _tokenCounter.increment();
@@ -71,7 +91,24 @@ contract AdvancedNft is ERC721("Advanced NFT", "ADV"), Ownable {
     // TODO: refactor: implement commit-reveal pattern for idx, instead of taking it from args
   }
 
+  function publicMint() public payable atStage(Stages.PublicSale) {
+    require(msg.value == MINT_PRICE, "Must send mint price");
+
+    uint256 _tokenId = _tokenCounter.current();
+
+    _tokenCounter.increment();
+
+    _safeMint(msg.sender, _tokenId);
+  }
+
   function merkleLeaf(uint256 _ticket) public view returns(bytes32) {
     return keccak256(abi.encode(msg.sender, _ticket));
+  }
+
+  function withdraw(address developer_) public {
+    _requireDeveloper(developer_);
+
+    (bool success, _) = payable(developer_).call{value: address(this).balance }("");
+    require(success);
   }
 }
